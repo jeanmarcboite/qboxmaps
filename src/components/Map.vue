ba4db69dee784ff828f7149ba8197a49387f10e8
Jean-Marc Boite <jeanmarcboite@gmail.com>
2018-06-06 13:07:43 +0200

    use sync

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -10,16 +10,23 @@ import OlMap from 'ol/map'
 import store from 'src/store'
 import interaction from 'ol/interaction'
 import dragAndDropInteraction from 'src/ol/interaction/dnd'
+import {
+  sync
+} from 'vuex-pathify'
 
 export default {
   name: 'Map',
   data() {
     return {}
   },
+  computed: {
+    ...sync('UI', ['layerSwitcherOpen']),
+  },
   methods: {
     onclick: function () {}
   },
   mounted: function () {
+    const self = this
     const map = new OlMap({
       target: 'map',
       interactions: interaction.defaults().extend([dragAndDropInteraction]),
@@ -27,7 +34,7 @@ export default {
     Vue.prototype.$map = map
     window.map = map
     map.on('click', function (event) {
-      store.commit('UI/closeDrawer')
+      self.layerSwitcherOpen = false
     })
   }
 }
