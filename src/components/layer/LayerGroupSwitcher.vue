66b3799e9cd6dbed3f79ad3ee795aebd668ac0e3
Jean-Marc Boite <jeanmarcboite@gmail.com>
2018-06-06 08:23:16 +0200

    added context-menu

--- src/components/layer/LayerGroupSwitcher.vue
+++ src/components/layer/LayerGroupSwitcher.vue
@@ -1,14 +1,14 @@
 <template>
 <q-collapsible indent icon-toggle>
   <template slot="header">
-      <q-checkbox v-model="visible" :label="group.get('title')"
+      <q-toggle v-model="visible" :label="group.get('title')"
         checked-icon="visibility"
         unchecked-icon="visibility_off"
         color="orange"
         @input="oninput"
       />
     </template>
-  <q-list highlight>
+  <q-list highlight inset-separator>
     <LayerSwitcher v-for="(layer, key) in layers" :ref="key" :key="key" :layer.sync="layer" />
   </q-list>
 </q-collapsible>
@@ -43,7 +43,7 @@ export default {
   methods: {
     oninput: function () {
       console.log('group: ' + this.group.get('title') + ((this.visible) ? ' visible' : ''))
-    }
+    },
   }
 }
 </script>
--- src/components/layer/LayerSwitcher.vue
+++ src/components/layer/LayerSwitcher.vue
@@ -1,8 +1,25 @@
 <template>
-<div>
-  <q-checkbox v-model="visible" :label="layer.get('title')" checked-icon="visibility" unchecked-icon="visibility_off" color="teal"
-    @input="oninput" />
-</div>
+<q-item @input="oninput">
+  <q-item-side>
+    <q-checkbox v-model="visible" checked-icon="visibility" unchecked-icon="visibility_off" color="teal" />
+  </q-item-side>
+  <q-context-menu>
+    <q-list link separator style="min-width: 150px; max-height: 300px;">
+      <q-item v-close-overlay>
+        <q-item-main label="Label" sublabel="Value" />
+      </q-item>
+
+      <q-item v-close-overlay>
+        <q-item-main label="Other Label" sublabel="Other Value" />
+      </q-item>
+    </q-list>
+  </q-context-menu>
+  <q-item-main>
+    <q-item-tile @input="oninput">
+      {{layer.get('title')}}
+    </q-item-tile>
+  </q-item-main>
+</q-item>
 </template>
 <script>
 export default {
