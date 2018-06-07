f103ad80a7c0c4518a825a5fbd815f7599ba36fa
Jean-Marc Boite <jeanmarcboite@gmail.com>
2018-06-07 08:42:02 +0200

    load files

--- src/components/layer/TrackSwitcher.vue
+++ src/components/layer/TrackSwitcher.vue
@@ -1,5 +1,11 @@
 <template>
 <div class="ol-unselectable ol-control ol-trackswitcher">
+  <q-context-menu>
+    <q-list highlight>
+      <LayerGroup v-for="(group, key) in groups()" v-if="group.get('trackLayer')" :key="key" :group="group" />
+    </q-list>
+  </q-context-menu>
+  <input type="file" :id="id" accept=".gpx" multiple style="display: none;" />
   <q-btn push small round @click="handleClick">
     <q-icon name="directions_walk" style="font-size: 1em;" />
   </q-btn>
@@ -17,6 +23,7 @@ import {
 } from 'vuex-pathify'
 
 import LayerGroup from './TrackLayerGroup.vue'
+import readFiles from 'src/ol/layer/readfiles'
 
 export default {
   components: {
@@ -25,12 +32,26 @@ export default {
   computed: {
     ...sync('UI', ['trackSwitcherOpen']),
   },
+  data: function () {
+    return {
+      id: 'TrackSwitcher/inputID'
+    }
+  },
+  mounted: function () {
+    document.getElementById(this.id).onchange = readFiles({
+      map: this.$map,
+      store: this.$store
+    })
+  },
   methods: {
     groups() {
       return (this.$map) ? this.$map.getLayers().getArray() : []
     },
     handleClick() {
-      this.trackSwitcherOpen = !this.trackSwitcherOpen
+      const el = document.getElementById(this.id)
+      if (el) {
+        el.click()
+      }
     }
   }
 }
