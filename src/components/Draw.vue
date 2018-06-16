4349cfde37eee2155ca89ebf904ea36b57f5b312
Jean-Marc Boite <jeanmarcboite@gmail.com>
2018-06-16 11:44:05 +0200

    modify features

--- src/components/Draw.vue
+++ src/components/Draw.vue
@@ -8,6 +8,12 @@
 </template>
 <script>
 import Draw from 'ol/interaction/draw'
+import Modify from 'ol/interaction/modify'
+import Snap from 'ol/interaction/snap'
+import Style from 'ol/style/style'
+import Fill from 'ol/style/fill'
+import Stroke from 'ol/style/stroke'
+import Circle from 'ol/style/circle'
 import VectorSource from 'ol/source/vector'
 import Track from 'src/ol/layer/Track'
 
@@ -27,27 +33,54 @@ export default {
   methods: {
     handleClick: function () {
       this.drawing = !this.drawing
+      
+      const style = new Style({
+          fill: new Fill({
+            color: 'rgba(255, 255, 255, 0.2)'
+          }),
+          stroke: new Stroke({
+            color: '#ffcc00',
+            width: 4
+          }),
+          image: new Circle({
+            radius: 7,
+            fill: new Fill({
+              color: '#ff0000'
+            })
+          })
+        })
+
       if (this.drawing) {
         console.log('startDraw')
 
         var source = new VectorSource({
           wrapX: false
         })
+      
+        const modify = new Modify({source: source})
+        this.$ol.map.addInteraction(modify)
 
         const draw = new Draw({
           source: source,
           freehand: false,
           type: 'LineString'
         })
+        
+        const snap = new Snap({source: source})
+        
         this.$ol.map.addInteraction(draw)
+        this.$ol.map.addInteraction(snap)
+        
         this.$ol.tracks.getLayers().push(new Track({
           title: 'New Track',
-          source
+          source,
+          style
         }))
         draw.on('drawend', (event) => {
           console.log('drawend')
           event.preventDefault()
           this.$ol.map.removeInteraction(draw)
+          this.$ol.map.removeInteraction(snap)
           this.drawing = false
         })
       }
